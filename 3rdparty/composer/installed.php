9fe9badd7c02a6db0cf461cea24474698c1fad65
Jörn Friedrich Dreyer <jfd@butonic.de>
2022-12-10 00:38:48 +0100

    bump getid3
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

---
+++ 3rdparty/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'butonic/autorename',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '466ed2ed759e608fd60dac2f97157e731298348e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'butonic/autorename' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '466ed2ed759e608fd60dac2f97157e731298348e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'james-heinrich/getid3' => array(
+            'pretty_version' => 'v1.9.22',
+            'version' => '1.9.22.0',
+            'reference' => '45f20faa0f0a24489740392c5b512ddcc36deccd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../james-heinrich/getid3',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
